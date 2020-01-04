7e0a303e8abe50f0e59d3f86a51c9245ed230e4b
celestialmk <martinsemakula1000@gmail.com>
2020-01-04 10:49:59 +0000

    adding static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_ROOT =  '/home/celestialsk/celestialsk.pythonanywhere.com/static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static', 'static_dirs'),
 )
